ae2a1fbcaa7f2658251c2c01c734db4ba5d7f3df
jordan <jordan.lesich@gmail.com>
2025-01-19 19:50:41 -0800

    write initial Sayeth Core contract

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+interface IReferrer {
+    function validatePost(address poster, bytes calldata content) external returns (bool);
+}
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+struct Record {
+    address poster;
+    bytes content;
+    address referrer;
+}
 
-    function increment() public {
-        number++;
+contract Sayeth {
+    event Say(address referrer, address poster, bytes content);
+    event Scribe(address referrer, address poster, bytes content, uint256 index);
+
+    Record[] public records;
+
+    function sayeth(address _referrer, bytes calldata _content, bool _store) public {
+        require(IReferrer(_referrer).validatePost(msg.sender, _content), "Post not validated by referrer");
+
+        if (_store) {
+            records.push(Record(msg.sender, _content, _referrer));
+            emit Scribe(_referrer, msg.sender, _content, records.length - 1);
+        } else {
+            emit Say(_referrer, msg.sender, _content);
+        }
     }
 }
