41e623c8c21a8b2eb2c763827fc5bffe3a07d6ac
jordan <jordan.lesich@gmail.com>
2025-01-20 00:18:37 -0800

    create simple staking contract for tests

--- src/referrers/StakeReferrer.sol
+++ src/referrers/StakeReferrer.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {IReferrer} from "../interfaces/IReferrer.sol";
-import {IERC20} from "lib/forge-std/src/interfaces/IERC20.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 struct Stake {
     uint256 amount;
@@ -10,7 +12,7 @@ struct Stake {
     bool isStaked;
 }
 
-contract StakeReferrer is IReferrer {
+contract StakeReferrer is ReentrancyGuard, Ownable, IReferrer {
     event Staked(address user, uint256 amount);
     event Withdrawn(address user, uint256 amount);
     event StakePermitted(address user, bool permitted);
@@ -23,15 +25,48 @@ contract StakeReferrer is IReferrer {
 
     mapping(address => Stake) public stakes;
 
-    constructor(address _stakingToken, uint256 _stakeAmt, uint256 _stakingPeriod) {
+    constructor(address _stakingToken, uint256 _stakeAmt, uint256 _stakingPeriod, address initialOwner)
+        Ownable(initialOwner)
+    {
         stakingToken = IERC20(_stakingToken);
         stakeAmt = _stakeAmt;
         stakingPeriod = _stakingPeriod;
     }
 
-    function stake(address _referrer) public payable {}
+    function stake(uint256 _amount) external nonReentrant {
+        require(_amount == stakeAmt, "Invalid stake amount");
+        require(stakes[msg.sender].isStaked == false, "Already staked");
 
-    function validatePost(address _sender, address _origin, bytes calldata _content) external override returns (bool) {
-        return true;
+        require(stakingToken.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+
+        stakes[msg.sender] = Stake(_amount, block.timestamp, true);
+
+        emit Staked(msg.sender, _amount);
+    }
+
+    function withdraw() external nonReentrant {
+        Stake storage userStake = stakes[msg.sender];
+        require(userStake.isStaked, "Not staked");
+        require(block.timestamp >= userStake.timestamp + stakingPeriod, "Staking period not over");
+
+        uint256 amount = userStake.amount;
+
+        delete stakes[msg.sender];
+
+        require(stakingToken.transfer(msg.sender, amount), "Transfer failed");
+
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function updateMinimumStake(uint256 _newAmt) external onlyOwner {
+        stakeAmt = _newAmt;
+    }
+
+    function updateStakingPeriod(uint256 _newPeriod) external onlyOwner {
+        stakingPeriod = _newPeriod;
+    }
+
+    function validatePost(address _sender, address, bytes calldata) public view returns (bool) {
+        return stakes[_sender].isStaked;
     }
 }
