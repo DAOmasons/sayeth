3fe9e22927a9478712e99390fd6bac187aa18675
jordan <jordan.lesich@gmail.com>
2025-02-19 22:43:36 -0800

    deploy hats referrers

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {stdJson} from "forge-std/StdJson.sol";
 import {Sayeth} from "src/Sayeth.sol";
 import {GGEmptyReferrer} from "src/referrers/GGEmptyReferrer.sol";
+import {GGHatsReferrer} from "src/referrers/GGHatsReferrer.sol";
 
 contract Deploy is Script {
     string _network;
@@ -15,6 +16,8 @@ contract Deploy is Script {
 
     string DEPLOYMENTS_DIR = string.concat(root, "/deployments/main.json");
 
+    mapping(string => uint256) adminIds;
+
     function run() external {
         uint256 pk = vm.envUint("PRIVATE_KEY");
         address deployer = vm.rememberKey(pk);
@@ -23,8 +26,10 @@ contract Deploy is Script {
 
         vm.startBroadcast(deployer);
 
-        _deploySayeth();
-        _deployEmptyReferrer();
+        // _deploySayeth();
+        // _deployEmptyReferrer();
+
+        _deployHatsReferrer();
 
         vm.stopBroadcast();
     }
@@ -52,6 +57,25 @@ contract Deploy is Script {
 
         console2.log("Deployed Sayeth at address: ", address(_emptyReferrer));
     }
-}
 
-// 0x0000000000000000000000000000000000004A75
+    function _deployHatsReferrer() internal {
+        adminIds["421614"] = 2372476540837674292835228943884614417292947446235469603522015213387776;
+        adminIds["42161"] = 2048956358079587954696203685355787570534049221797049916958840216092672;
+
+        uint256[] memory hatIds = new uint256[](1);
+        address hatsAddress = 0x3bc1A0Ad72417f2d411118085256fC53CBdDd137;
+
+        uint256 networkAdminId = adminIds[_network];
+
+        if (networkAdminId == 0) {
+            console2.log("No admin id found for network: ", _network);
+            return;
+        }
+        hatIds[0] = networkAdminId;
+
+        GGHatsReferrer hatsRefferer = new GGHatsReferrer(hatIds, hatsAddress, networkAdminId);
+
+        console2.log("Deployed GGHatsReferrer at address: ", address(hatsRefferer));
+    }
+}
+// 0x3bc1A0Ad72417f2d411118085256fC53CBdDd137
